06aba77749be083243afba04561a112cd1ba46cd
Ryan Jahnige <46071689+rsjahnige@users.noreply.github.com>
2024-05-30 07:47:02 -0400

    Start of menu updates

--- Menu.h
+++ Menu.h
@@ -1,12 +1,3 @@
-/*  
-  This could be implemented as a Linked List so that user can add and remove 
-  options without reinitializing the array each time (e.g., when dealing with alarms).
-  Would the effort to restructure this class outweigh the benifits of such 
-  an implementation?
-    - Using a templated class could be benificial for when an Alarm class is defined
-      - How will a character sting vs an Alarm object be printed to the LCD ??
-*/
-
 #ifndef MENU_H
 #define MENU_H
 
@@ -15,28 +6,38 @@
 
 #define MENU    0x01
 
-class Menu : public Context 
+namespace UI
 {
-public:
-  Menu(const LiquidCrystal *lcd, uint8_t num_options=3, char **options=nullptr);
-  ~Menu();
+  template<class T>
+  class Node
+  {
+  public:
+    Node();
+    ~Node();
 
-  virtual void display(void) override;
+  private:
+    T _data;
+    Node<T> *_next_link;      // Forward iteration
+    Node<T> *_last_link;      // Reverse iteration
+  };
+  
+  template<class T>
+  class Menu : public Context 
+  {
+  public:
+    Menu(const LiquidCrystal *lcd, uint8_t num_options=3, char **options=nullptr);
+    ~Menu();
+  
+    virtual void display(void) override;
+  
+    virtual void shiftLeft(void) override { return; }
+    virtual void shiftRight(void) override { return; }
+    virtual void shiftDown(void) override;
+    virtual void shiftUp(void) override;
 
-  virtual void shiftLeft(void) override { return; }
-  virtual void shiftRight(void) override { return; }
-  virtual void shiftDown(void) override;
-  virtual void shiftUp(void) override;
-
-protected:
-  /*** !!! Untested functions !!! ***/
-  virtual void addOption(char *option) final;
-  virtual void rmOption(uint8_t index) final;
-
-private:
-  char **_options;
-  uint8_t _size;
-  uint8_t _index;       // Array index of first visible line
+  private:
+    Node<T> *_head;
+  };
 };
 
 #endif
