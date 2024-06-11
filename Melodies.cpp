6dfce37fae757520cd9784ce091f8867eaa3133a
Ryan Jahnige <46071689+rsjahnige@users.noreply.github.com>
2024-06-11 19:54:57 -0400

    Linked user defined alarms to the buzzer and clock

--- Melodies.cpp
+++ Melodies.cpp
@@ -5,21 +5,19 @@ namespace Database
   const Melody Default = {
     "Default\0",
     8,
-    //{NOTE_C4, NOTE_G3, NOTE_G3, NOTE_A3, NOTE_G3, REST, NOTE_B3, NOTE_C4},
-    nullptr,
-    new uint8_t[8]{4, 8, 8, 4, 4, 4, 4, 4},
-    60
+    60,
+    new unsigned int[8]{NOTE_C4, NOTE_G3, NOTE_G3, NOTE_A3, NOTE_G3, REST, NOTE_B3, NOTE_C4},
+    new unsigned int[8]{4, 8, 8, 4, 4, 4, 4, 4}
   };
   
   const Melody Allegro = {
     "Allegro\0",
     23,
-    //{NOTE_C4, NOTE_C4, NOTE_G3, NOTE_G3, NOTE_G3, REST, NOTE_GS3, NOTE_GS3, NOTE_GS3, REST,
-    //  NOTE_F4, NOTE_F4, NOTE_F4, NOTE_D4, NOTE_D4, NOTE_G4, NOTE_G4, NOTE_F4, NOTE_DS4, 
-    //  NOTE_D4, NOTE_G4, NOTE_G4, NOTE_DS4},
-    nullptr,
-    new uint8_t[23]{1,4,3,3,3,4,3,3,3,4,3,3,3,1,4,4,4,3,1,4,4,4,1},
-    50
+    50,
+    new unsigned int[23]{NOTE_C4, NOTE_C4, NOTE_G3, NOTE_G3, NOTE_G3, REST, NOTE_GS3, NOTE_GS3, 
+                          NOTE_GS3, REST, NOTE_F4, NOTE_F4, NOTE_F4, NOTE_D4, NOTE_D4, NOTE_G4, 
+                          NOTE_G4, NOTE_F4, NOTE_DS4, NOTE_D4, NOTE_G4, NOTE_G4, NOTE_DS4},
+    new unsigned int[23]{1,4,3,3,3,4,3,3,3,4,3,3,3,1,4,4,4,3,1,4,4,4,1}
   };
 
   const Melody* getNextMelody(const Melody* current) 
